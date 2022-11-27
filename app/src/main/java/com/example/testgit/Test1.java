3788c7d9553b006057de6567e012e9ce9c617103
yanabirukova <y.birukova@gmail.com>
2022-11-27 12:37:33 +0300

    second

--- app/src/main/java/com/example/testgit/Test1.java
+++ app/src/main/java/com/example/testgit/Test1.java
@@ -1,8 +1,11 @@
 package com.example.testgit;
 
+import java.util.Random;
+
 public class Test1 {
     public static void main(String[] args) {
-        
+        Random random = new Random();
+        int simArraySize = random.nextInt(10) + 1;
     }
 
 }
